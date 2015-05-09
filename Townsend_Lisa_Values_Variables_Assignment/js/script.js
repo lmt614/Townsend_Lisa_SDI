69a48bec3faf2c6206e77aafc61b584bffd98931
Lisa Townsend <lmt614@fullsail.edu>
2015-05-09 17:32:09 -0500

    Used variable to access array
    Used variable to access array

--- Townsend_Lisa_Values_Variables_Assignment/js/script.js
+++ Townsend_Lisa_Values_Variables_Assignment/js/script.js
@@ -5,4 +5,6 @@ var muppetNames = ["Kermit", "Miss Piggy", "Gonzo", "Rizzo"]; //0,1,2,3
 muppetNames[1] = "Rizzo"; //change index #1 to Rizzo
 muppetNames[2] = "Fozzy"; //added item to array
 
+var num = 2; //array accessed using variable
+
 console.log(muppetNames[2]); //let me see whats in the index #1
