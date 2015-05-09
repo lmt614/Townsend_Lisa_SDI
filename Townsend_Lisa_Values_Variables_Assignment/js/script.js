723e1ea50fd424b11a9b374ba5f995e178309a05
Lisa Townsend <lmt614@fullsail.edu>
2015-05-09 17:26:13 -0500

    Changed index number 1 to Rizzo and commented
    Changed index number 1 to Rizzo and commented

--- Townsend_Lisa_Values_Variables_Assignment/js/script.js
+++ Townsend_Lisa_Values_Variables_Assignment/js/script.js
@@ -2,4 +2,6 @@
 
 var muppetNames = ["Kermit", "Miss Piggy", "Gonzo", "Rizzo"]; //0,1,2,3
 
+muppetNames[1] = "Rizzo"; //change index #1 to Rizzo
+
 console.log(muppetNames[1]); //let me see whats in the index #1
