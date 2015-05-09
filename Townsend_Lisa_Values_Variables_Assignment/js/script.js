941a9dded7249ccacbb9ba8c2e90f546d4bd31b0
Lisa Townsend <lmt614@fullsail.edu>
2015-05-09 17:09:00 -0500

    Declared array and assigned collection
    Declared array and assigned collection

--- Townsend_Lisa_Values_Variables_Assignment/js/script.js
+++ Townsend_Lisa_Values_Variables_Assignment/js/script.js
@@ -1,10 +1,3 @@
+//Arrays
 
-// Variables of Different Types, Numbers, Booleans and Strings
-
-var age = 24; //Number variable
-
-var isStudent = false; //Boolean Variable
-
-var firstName = "Lisa";
-
-var phrase = 'I don\'t know \n You\'ll have to speak with the professor';
+var muppetNames = ["Kermit", "Miss Piggy", "Gonzo", "Rizzo"];
