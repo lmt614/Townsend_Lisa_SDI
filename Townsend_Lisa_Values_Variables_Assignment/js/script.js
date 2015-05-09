20006ceb3461ca4cb1abf4041e24c00213a3d548
Lisa Townsend <lmt614@fullsail.edu>
2015-05-09 16:30:19 -0500

    Showed that you can declare and define on same line
    Showed that you can declare and define on same line

--- Townsend_Lisa_Values_Variables_Assignment/js/script.js
+++ Townsend_Lisa_Values_Variables_Assignment/js/script.js
@@ -2,5 +2,7 @@
 //Defining Variables
 
 var age; //declaration
-age = 24; //definition 
+age = 24; //definition
 //storage container = value
+
+var yearOfBirth = 1990; //declaring and defining on the same line
