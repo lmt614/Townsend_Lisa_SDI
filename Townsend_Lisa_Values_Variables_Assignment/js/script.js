461e794a596dcdbdcb4e61bb2ba78af6f92818a4
Lisa Townsend <lmt614@fullsail.edu>
2015-05-09 17:28:27 -0500

    Tested with console

--- Townsend_Lisa_Values_Variables_Assignment/js/script.js
+++ Townsend_Lisa_Values_Variables_Assignment/js/script.js
@@ -5,4 +5,4 @@ var muppetNames = ["Kermit", "Miss Piggy", "Gonzo", "Rizzo"]; //0,1,2,3
 muppetNames[1] = "Rizzo"; //change index #1 to Rizzo
 muppetNames[2] = "Fozzy"; //added item to array
 
-console.log(muppetNames[1]); //let me see whats in the index #1
+console.log(muppetNames[2]); //let me see whats in the index #1
