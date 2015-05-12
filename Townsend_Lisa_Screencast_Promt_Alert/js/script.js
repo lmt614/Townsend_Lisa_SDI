93c25a6f3d91f3df28b447896c1996f3fa9fa21f
Lisa Townsend <lmt614@fullsail.edu>
2015-05-12 18:58:45 -0500

    Collect width and height and calculate area of a rectangle
    Collect information from prompt and calculates information and prints
    out to console.

--- Townsend_Lisa_Screencast_Promt_Alert/js/script.js
+++ Townsend_Lisa_Screencast_Promt_Alert/js/script.js
@@ -5,5 +5,10 @@
  Prompt and Alert
  */
 
-var userInput = prompt("Enter your year of birth:");
-console.log(userInput);
+//var userInput = prompt("Enter your year of birth:");
+//console.log(userInput);
+
+var width = prompt("We are calculating the area of a rectangle. \nPlease enter the width");
+var height = prompt("Please enter the height");
+var area = width * height;
+console.log(area);
