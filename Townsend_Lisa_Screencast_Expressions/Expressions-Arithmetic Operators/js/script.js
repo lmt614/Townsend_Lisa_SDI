c574a47d94065af75ffa9a9184220b31f9441ccc
Lisa Townsend <lmt614@fullsail.edu>
2015-05-11 20:55:58 -0500

    Added width times height and tested
    Added width times height and tested to see if it was multiplying them
    to give the sum of twenty.

--- Townsend_Lisa_Screencast_Expressions/Expressions-Arithmetic Operators/js/script.js
+++ Townsend_Lisa_Screencast_Expressions/Expressions-Arithmetic Operators/js/script.js
@@ -4,3 +4,9 @@
  Section # 01
  Expressions- Arithmetic Operators
  */
+
+//area of the triangle is half of the width times the height
+var width = 4;
+var height = 5;
+var area = width * height;
+console.log(area);
