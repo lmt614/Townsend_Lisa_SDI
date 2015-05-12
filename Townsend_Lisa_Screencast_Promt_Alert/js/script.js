fa8d5e6a481c9ac2bf2221a0caca385579e9e737
Lisa Townsend <lmt614@fullsail.edu>
2015-05-12 18:59:27 -0500

    Added comments
    Added comments

--- Townsend_Lisa_Screencast_Promt_Alert/js/script.js
+++ Townsend_Lisa_Screencast_Promt_Alert/js/script.js
@@ -10,5 +10,5 @@
 
 var width = prompt("We are calculating the area of a rectangle. \nPlease enter the width");
 var height = prompt("Please enter the height");
-var area = width * height;
-console.log(area);
+var area = width * height; //calculates with that info
+console.log(area); //prints it out to console
