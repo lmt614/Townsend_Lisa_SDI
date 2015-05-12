24ff4397c3ce92a1310d8e50e13a5accaf83c919
Lisa Townsend <lmt614@fullsail.edu>
2015-05-11 23:11:24 -0500

    Created variables for quiz grades. Added comment
    Created variables for quiz grades. Added comment to write out a plan
    for what needs to be done.

--- Townsend_Lisa_Screencast_Expressions/Expressions-PEMDAS/js/script.js
+++ Townsend_Lisa_Screencast_Expressions/Expressions-PEMDAS/js/script.js
@@ -4,3 +4,10 @@
  Section # 01
  Expressions- PEMDAS - The Order of Operations
  */
+
+var quiz1 = 87;
+var quiz2 = 100;
+var quiz3 = 60;
+var quiz4 = 80;
+
+//the average is the sum of the items divided by the number of items
