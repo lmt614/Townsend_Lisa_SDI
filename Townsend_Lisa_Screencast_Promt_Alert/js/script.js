4b77ba998327adb9800dc6b254ad27825ac0ded7
Lisa Townsend <lmt614@fullsail.edu>
2015-05-12 19:07:18 -0500

    Could use variable and add concatenation to it instead
    Could use variable and add concatenation to it instead

--- Townsend_Lisa_Screencast_Promt_Alert/js/script.js
+++ Townsend_Lisa_Screencast_Promt_Alert/js/script.js
@@ -12,5 +12,5 @@ var width = prompt("We are calculating the area of a rectangle. \nPlease enter t
 var height = prompt("Please enter the height");
 var area = width * height; //calculates with that info
 //console.log(area); //prints it out to console
-
-alert("The area of your rectangle is" + area + "sq feet");
+var result = "The area of your rectangle is " + area + " sq feet";
+alert(result);
