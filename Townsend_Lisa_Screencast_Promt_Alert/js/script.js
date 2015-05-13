954f369e4e3d5e0c12fd46e640c3af77527b30dd
Lisa Townsend <lmt614@fullsail.edu>
2015-05-12 19:04:02 -0500

    Another way to show results to user
    Another way to show results to user with the alert function.

--- Townsend_Lisa_Screencast_Promt_Alert/js/script.js
+++ Townsend_Lisa_Screencast_Promt_Alert/js/script.js
@@ -11,4 +11,6 @@
 var width = prompt("We are calculating the area of a rectangle. \nPlease enter the width");
 var height = prompt("Please enter the height");
 var area = width * height; //calculates with that info
-console.log(area); //prints it out to console
+//console.log(area); //prints it out to console
+
+alert("The area of your rectangle is" + area + "sq feet");
