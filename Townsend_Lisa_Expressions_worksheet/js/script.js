a4ffba8e76f00fcc35ca3ae4ce126747af9fe0b7
Lisa Townsend <lmt614@fullsail.edu>
2015-05-14 00:13:14 -0500

    Updated date and added solved dog years problem
    Updated date and added solved dog years problem

--- Townsend_Lisa_Expressions_worksheet/js/script.js
+++ Townsend_Lisa_Expressions_worksheet/js/script.js
@@ -1,6 +1,17 @@
 /**
  Lisa Townsend
- 5/12/2015
+ 5/13/2015
  Section # 01
  Assignment Name: Expressions Worksheet
  */
+
+//Dog years
+
+//Declared variable
+var sparkysAge = prompt("Calculating your dogs human age in dog years. \nEnter age:");
+//Dog years is the constant
+const dogYears = 7;
+//Take the constant 7 and multiply it by dogs age entered
+var dogYearAge = sparkysAge * dogYears;
+//Print out age entered and result in dog years
+console.log("Sparky is " + sparkysAge + " human years old, which is " +  dogYearAge  + " in dog years.");
