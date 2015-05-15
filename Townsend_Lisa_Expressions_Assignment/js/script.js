cff3a3fd578000efb584654df12d66e66ed7f35a
Lisa Townsend <lmt614@fullsail.edu>
2015-05-14 21:18:14 -0500

    Added comment to organize code and added alert
    Added comment to organize code. Added alert to tell the user what we
    are trying to do before we give them a prompt.

--- Townsend_Lisa_Expressions_Assignment/js/script.js
+++ Townsend_Lisa_Expressions_Assignment/js/script.js
@@ -6,3 +6,8 @@
  */
 
 //Between work, school and sleep, how many hours left are there to exercise?
+
+//Alerts, variables and prompts
+
+//Alert user to let them know what it is we are trying to accomplish and what we need. They will then understand why we are asking them questions.
+alert("In a 24 hour day, after work, school and sleeping, how many hours do you have left to exercise? \nLets find out!");
