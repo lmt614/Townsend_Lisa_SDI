fc56c1649288a3c61440e15d2a5cc1070eee5cd5
Lisa Townsend <lmt614@fullsail.edu>
2015-05-16 13:30:08 -0500

    Coditional Logic- relational expressions
    Conditional Logic- relational expressions

--- Conditionals/js/script.js
+++ Conditionals/js/script.js
@@ -6,8 +6,9 @@
  */
 
 
-
+/**
 //Conditional Logic
+
 
 //If a child is old enough to ride a roller coaster
 
@@ -23,4 +24,22 @@ var oldEnough = true;
 if(oldEnough){
     //code performed if condition is true
     console.log("You can ride the coaster!");
+}
+*/
+
+//Conditional Logic- Relational Expressions
+
+//Kids over 48 inches are able to ride roller coaster, but under 48 inches, they're not.
+
+//Store kid height in variable
+
+var kidHeight = 30; //Kid is 30 inches tall
+
+//If the child is old enough, print to the console "You can ride!"
+//If the kid is over 48 inches in height. If the kidHeight is > (greater than)
+
+if(kidHeight > 48){
+    //code performed if condition is true
+    console.log("You can ride the coaster!");
+
 }
