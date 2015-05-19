21d42dddb1dfdb95564f3ce9171501862159e9ad
Lisa Townsend <lmt614@fullsail.edu>
2015-05-18 21:11:53 -0500

    Added another variable. Added another condition
    Added another variable. Added another condition.

--- Conditionals/js/script.js
+++ Conditionals/js/script.js
@@ -90,13 +90,19 @@ if(kidHeight > minHeight){
 
 var kidHeight = 52;
 var minHeight = 48;
+var wParentHeight = 45; //The height of the kid with the parent
 
 //If the child is old enough, print to the console "you can ride!"
 //If the kid is over 48 inches in height
 if(kidHeight > minHeight){
-    //code performed if condition is true
+    //You can ride!
     console.log("You can ride the coaster!");
+}else if(kidHeight > wParentHeight ){
+    //You can ride with a parent present
+    console.log("You can ride, but only with a parent present.");
 }else{
-    //code performed if condition is false
+    //Sorry you have growing to do
     console.log("Sorry kid, you've got some growing to do first!");
 }
+
+
