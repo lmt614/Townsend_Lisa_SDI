05d5113c6cd34e63373d5a901f96a12542119cdb
Lisa Townsend <lmt614@fullsail.edu>
2015-05-18 23:07:05 -0500

    Conditions if and else
    Conditions if and else

--- Conditionals/js/script.js
+++ Conditionals/js/script.js
@@ -114,4 +114,8 @@ var budget = 300;
 var iPhonePrice = 199.99;
 var payCheck = 200;
 
-
+if(iPhonePrice < budget){
+    console.log("We can buy the phone!");
+}else{
+    console.log("No phone for you!!");
+}
