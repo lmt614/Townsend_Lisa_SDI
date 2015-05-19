be07f5097aab02ca955cb558619e1a87b0f9b108
Lisa Townsend <lmt614@fullsail.edu>
2015-05-18 20:47:16 -0500

    Used else instead of second if statement
    Used else instead of second if statement. If  previous statement is
    false do this.

--- Conditionals/js/script.js
+++ Conditionals/js/script.js
@@ -79,12 +79,7 @@ var minHeight = 48;
 if(kidHeight > minHeight){
     //code performed if condition is true
     console.log("You can ride the coaster!");
-}
-
-//If statement less than
-//If child is less than min height < then print "Sorry, kid, you've got some growing to do first!"
-//Changed to if it is the min height or less <=
-
-if(kidHeight <= minHeight){
+}else{
+    //code performed if condition is false
     console.log("Sorry kid, you've got some growing to do first!");
 }
