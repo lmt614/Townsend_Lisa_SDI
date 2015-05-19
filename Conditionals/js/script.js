654b2edba7cf2e9c38710b8effd1fafca6c3d4f7
Lisa Townsend <lmt614@fullsail.edu>
2015-05-18 23:37:25 -0500

    OR operator goes in between booleans
    OR operator goes in between booleans so you don’t need to have strict
    equality restate it

--- Conditionals/js/script.js
+++ Conditionals/js/script.js
@@ -135,7 +135,7 @@ var wonLottery = true;
 
 //If the price of the phone is less that our budget AND if our paycheck is over 300
 
-if(iPhonePrice < budget || wonLottery === true){
+if(iPhonePrice < budget || wonLottery){
     console.log("We can buy the phone!");
 }else{
     console.log("No phone for you!!");
