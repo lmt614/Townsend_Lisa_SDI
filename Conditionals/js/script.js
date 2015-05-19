252cb7c7c16e745dfaa7cdf0e3bfcd10e830da0d
Lisa Townsend <lmt614@fullsail.edu>
2015-05-18 21:02:05 -0500

    Copied from previous code starting Else If
    Copied from previous code starting Else If screencast.

--- Conditionals/js/script.js
+++ Conditionals/js/script.js
@@ -68,6 +68,7 @@ if(kidHeight + sneakerLift > minHeight){
 }
 */
 
+/**
 //Conditional Logic- If and Else
 
 var kidHeight = 52;
@@ -76,6 +77,22 @@ var minHeight = 48;
 
 //If the child is old enough, print to the console "you can ride!"
 //If the kis is over 48 inches in height
+if(kidHeight > minHeight){
+    //code performed if condition is true
+    console.log("You can ride the coaster!");
+}else{
+    //code performed if condition is false
+    console.log("Sorry kid, you've got some growing to do first!");
+}
+ */
+
+//Conditional Logic- Else If
+
+var kidHeight = 52;
+var minHeight = 48;
+
+//If the child is old enough, print to the console "you can ride!"
+//If the kid is over 48 inches in height
 if(kidHeight > minHeight){
     //code performed if condition is true
     console.log("You can ride the coaster!");
