c0e2f44638f7289ad6411d8f42b01bee4f8b9a68
Lisa Townsend <lmt614@fullsail.edu>
2015-05-21 23:34:03 -0500

    Added comment and alert to explain
    Added comment and alert to explain what I was planning and why I am
    about to prompt the user

--- Townsend_Lisa_Conditionals_Assignment/js/script.js
+++ Townsend_Lisa_Conditionals_Assignment/js/script.js
@@ -4,3 +4,7 @@
  Date: 05/21/15
  Title: Conditionals Assignment
  */
+
+//Singing competition being held at Coren Hall Solos and choirs are welcomed. Ticket price for entry is $20 plus tax for solos and $30 plus tax for choirs. Both solos will be given different room numbers for auditions which will also be at Coren Hall this Friday.
+
+alert("Register today for singing competition at Coren Hall! Only Soloists and choirs qualify.");
