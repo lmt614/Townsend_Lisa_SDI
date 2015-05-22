c26066fbfff8c7b2f6e38585834308a438ee8aba
Lisa Townsend <lmt614@fullsail.edu>
2015-05-21 23:35:06 -0500

    Prompted user for name and validated prompt
    Prompted user for name and validated prompt

--- Townsend_Lisa_Conditionals_Assignment/js/script.js
+++ Townsend_Lisa_Conditionals_Assignment/js/script.js
@@ -8,3 +8,13 @@
 //Singing competition being held at Coren Hall Solos and choirs are welcomed. Ticket price for entry is $20 plus tax for solos and $30 plus tax for choirs. Both solos will be given different room numbers for auditions which will also be at Coren Hall this Friday.
 
 alert("Register today for singing competition at Coren Hall! Only Soloists and choirs qualify.");
+
+//prompting user for name
+var userName = prompt("What is your name or choir's name?");
+
+//Prompt validation
+if(userName ===""){
+ 
+ //re-prompt the user
+ userName = prompt("Please do not leave blank!\n How many do you have in your group?");
+}
