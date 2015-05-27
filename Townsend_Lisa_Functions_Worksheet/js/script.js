a402b2428f8506a19411aaaad85864c21721fe49
Lisa Townsend <lmt614@fullsail.edu>
2015-05-27 13:28:52 -0500

    Added problem 1
    Added problem 1 found the circumference of the circle with a radius of
    7. Tested results with calculator

--- Townsend_Lisa_Functions_Worksheet/js/script.js
+++ Townsend_Lisa_Functions_Worksheet/js/script.js
@@ -7,3 +7,23 @@
 
 //alert("Test Connection");
 
+
+
+//Problem 1 - Circumference
+
+//Calculate the circumference of a circle
+
+//circumferenceOfCircle holds variable Radius
+function circumferenceOfCircle(Radius){
+    //code the function runs
+    const pi = 3.14; //constant is pi
+    var circumferenceOfCircle = 2 * 3.14 * Radius; //calculation and var that will store info
+    console.log("The circumference of the circle is " +circumferenceOfCircle+ ".");
+}
+
+circumferenceOfCircle(7); //Function call
+
+/*
+Test result: Answer = 43.96 for the circumference of the circle
+ */
+
