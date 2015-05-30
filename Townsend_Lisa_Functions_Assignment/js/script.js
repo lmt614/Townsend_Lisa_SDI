ca6f028630872da463bc21314a0c9e0c36e7fb09
Lisa Townsend <lmt614@fullsail.edu>
2015-05-29 22:19:19 -0500

    Commented explanation and alerted the user
    Commented explanation and alerted the user

--- Townsend_Lisa_Functions_Assignment/js/script.js
+++ Townsend_Lisa_Functions_Assignment/js/script.js
@@ -5,5 +5,8 @@
  Assignment Name: Functions Assignment
  */
 
-//Test Connection
-alert("Test Connection");
+//Selling carpet for 15.99 per sq yd. Prompting users for the length and width of their room, that needs carpet, to find the area and price of carpet.
+
+
+//Alert user to explain what you are doing
+alert("Need carpet? Tell us the length and width of the room and we will tell you how much it will cost.");
